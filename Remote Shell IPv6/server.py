eb4e7af9873ba7d57cf1b70bb6ffb42cf5237ec0
Agustín Montaña <81200417+Agustinm28@users.noreply.github.com>
2022-10-06 18:52:17 -0300

    ipv6_update

--- Remote Shell IPv6/server.py
+++ Remote Shell IPv6/server.py
@@ -2,20 +2,27 @@ from asyncio import sslproto, subprocess
 import socketserver as ss
 import socket
 import argparse
-import pickle as p
-import sys
-import math
 from subprocess import Popen, PIPE
 import subprocess
+import threading
 
 
-class ThreadingTCPServer(ss.ThreadingMixIn, ss.TCPServer):
-        pass
-
-
-class ForkingTCPServer(ss.ForkingMixIn, ss.TCPServer):
+class ForkedTCPServer4(ss.ForkingMixIn, ss.TCPServer):
+    address_family = socket.AF_INET
     pass
 
+class ForkedTCPServer6(ss.ForkingMixIn, ss.TCPServer):
+    address_family = socket.AF_INET6
+    pass
+
+
+class ThreadedTCPServer4(ss.ThreadingMixIn, ss.TCPServer):
+    address_family = socket.AF_INET
+    pass
+
+class ThreadedTCPServer6(ss.ThreadingMixIn, ss.TCPServer):
+    address_family = socket.AF_INET6
+    pass
 
 class TCPRequestHandler(ss.BaseRequestHandler):
 
@@ -40,34 +47,52 @@ class TCPRequestHandler(ss.BaseRequestHandler):
                 output += subprocess.getoutput(command)
                 self.request.send(output.encode(FORMAT))
 
-def main():
+def args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--port', type=int, default=0, help='puerto del servidor')
     parser.add_argument('-c', '--conc', required=True, choices=['p', 't'], help='tipo de concurrencia: multi-process (p), multi-threads (t)')
     args = parser.parse_args()
 
-    server(args)
+    return args
+
+def server_p(d):
+    ss.TCPServer.allow_reuse_address = True
+    if d[0] == socket.AF_INET:
+        with ForkedTCPServer4(d[4], TCPRequestHandler) as server:
+            server.serve_forever()
+    
+    elif d[0] == socket.AF_INET6:
+        with ForkedTCPServer6(d[4], TCPRequestHandler) as server:
+                server.serve_forever()
 
 
-def server(args):
+def server_t(d):
+    ss.TCPServer.allow_reuse_address = True
+    if d[0] == socket.AF_INET:
+        with ThreadedTCPServer4(d[4], TCPRequestHandler) as server:
+            server.serve_forever()
+    
+    elif d[0] == socket.AF_INET6:
+        with ThreadedTCPServer6(d[4], TCPRequestHandler) as server:
+                server.serve_forever()
 
-    HOST, PORT = 'localhost', args.port
+def main(args):
+
     options = {'t': ss.ThreadingTCPServer, 'p': ss.ForkingTCPServer}
-    addr = socket.getaddrinfo("localhost", 5000, socket.AF_UNSPEC, socket.SOCK_STREAM)
-    print(addr)
-    if addr[0] == socket.AF_INET:
-        with options.get(args.conc)((HOST, PORT), TCPRequestHandler) as server:
-            print(f'[WAITING] Server is waiting for connections on {HOST}')
+    addrs = []
+    addrs.append(socket.getaddrinfo("localhost", args.port, socket.AF_INET, 1)[0])
+    addrs.append(socket.getaddrinfo("localhost", args.port, socket.AF_INET6, 1)[0])
+    
+    if args.conc == 'p':
+        for d in addrs:
+            print(f'[WAITING] Process Server is waiting for connections on {d[4]}')
+            threading.Thread(target=server_p, args=(d,)).start()
 
-            server.serve_forever()
-    else:
-        address_family = socket.AF_INET6
-        with options.get(args.conc)((HOST, PORT), TCPRequestHandler) as server:
-            print(f'[WAITING] Server is waiting for connections on {HOST}')
-
-            server.serve_forever()
-
+    elif args.conc == 't':
+        for d in addrs:
+            print(f'[WAITING] Threading Server is waiting for connections on {d[4]}')
+            threading.Thread(target=server_t, args=(d,)).start()
 
 if __name__ == "__main__":
-    main()
+    main(args())
 
