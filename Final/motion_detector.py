ea6e47b18daf1961891738b3824f29f91bb9a22d
Agustinm28 <agustinm28@gmail.com>
2023-04-13 18:11:15 -0300

    motion code

---
+++ Final/motion_detector.py
@@ -0,0 +1,19 @@
+import cv2
+
+cap = cv2.VideoCapture('video.mp4')
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    fgmask = fgbg.apply(frame)
+    cv2.imshow('frame', fgmask)
+
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
